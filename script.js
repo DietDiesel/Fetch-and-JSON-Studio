5d61b03f47ed78303d4ebd5777447b24b5f8892b
swankidelic <swankidelic@gmail.com>
2021-08-03 09:43:33 -0500

    extra credit bonus missions complete

--- script.js
+++ script.js
@@ -1,28 +1,58 @@
 let roster = "";
 let rosterSkills = "";
+let statusHighlight = "";
+let astronautStatus = "Unknown";
+let i = 0;
+let j = 0;
+
+//trying a sort function on the json object
+//blatantly stolen from https://medium.com/@asadise/sorting-a-json-array-according-one-property-in-javascript-18b1d22cd9e9
+
+function sortByProperty(property){  
+    return function(a,b){  
+       if(a[property] > b[property])  
+          return -1;  
+       else if(a[property] < b[property])  
+          return 1;  
+   
+       return 0;  
+    }  
+ }
 
 window.addEventListener("load", function() {
     fetch("https://handlers.education.launchcode.org/static/astronauts.json").then( function(response) {
         response.json().then( function(json) {
-            let i = 0;
-            let j = 0;
+
+            //trying a sort function on the json object
+            json.sort(sortByProperty("hoursInSpace"));
+
             for (i in json) {
                 //putting the skills array into a string of HTML, for use later in roster
+                    //first reset the string
                 rosterSkills = "";
+                    //then loop through the skills and concatenate
                 for (j of json[i].skills) {
                     rosterSkills += j + ', ';
                 }
+                    //trim off the last comma and space
                 rosterSkills = rosterSkills.slice(0, rosterSkills.length -2);
+
                 //toggle status based on active entry
-                let astronautStatus = "Unknown";
                 switch (json[i].active) {
                     case true:
                         astronautStatus = "Active";
                         break;
-                    case false:
+                    default:
                         astronautStatus = "Inactive";
-                        break;
                 }
+
+                //extra styling for active astronauts
+                if (astronautStatus === "Active") {
+                    statusHighlight = ' style="color: limegreen; font-weight: bold;"';
+                } else {
+                    statusHighlight = "";
+                }
+
                 //build the HTML for this roster entry
                 roster += `
                 <div id="roster">
@@ -38,7 +68,7 @@ window.addEventListener("load", function() {
                         </div>
                         <div id="innerColumn">
                             <div id="plainTitle">ID#<div id="plainText">${json[i].id}</div></div>
-                            <div id="plainTitle">Status<div id="plainText">${astronautStatus}</div></div>
+                            <div id="plainTitle">Status<div id="plainText"${statusHighlight}>${astronautStatus}</div></div>
                             <div id="plainTitle">Flight Hours<div id="plainText">${json[i].hoursInSpace}</div></div>
                         </div>
                         <div id="innerColumn">
@@ -48,11 +78,11 @@ window.addEventListener("load", function() {
                 </div>
                 `
             }
+
+            //finally apply the HTML to the page
+            roster = '<div id="fancyName">' + json.length + ' total astronauts in roster</div>' + roster;
             document.getElementById("container").innerHTML = roster;
         });
-
-
-
     });
     
 
